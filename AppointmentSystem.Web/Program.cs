4574db06c6d857ddee3eb28e1bf7f0ae8611112f
mehak <dhawanmehak10@gmail.com>
2025-03-23 13:28:21 +0530

    add db

--- AppointmentSystem.Web/Program.cs
+++ AppointmentSystem.Web/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppointmentDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("VotingAppConnectionString")
+options.UseSqlServer(builder.Configuration.GetConnectionString("AppointmentSystemConnectionString")
     )); 
 var app = builder.Build();
 
